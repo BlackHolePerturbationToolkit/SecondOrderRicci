d69290bdf40c97561ffa0e406a93ae454bb71c92
Barry Wardell <barry.wardell@gmail.com>
2014-07-04 14:33:58 -0400

    Fixup h5wrapper

--- h5wrapper.cc
+++ h5wrapper.cc
@@ -1,18 +1,9 @@
 /*
- * Copyright 2010-2011 Barry Wardell and Ian Hinder
+ * Copyright 2014 Barry Wardell
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * This file is distributed under the University of Illinois/NCSA
+ * Open Source License. See LICENSE file for details.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this library; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #include "h5wrapper.h"
