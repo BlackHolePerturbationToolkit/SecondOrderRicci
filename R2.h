c3d506ad4d382f8f2f0b1ee9840e0e81dfaa6bff
Barry Wardell <barry.wardell@gmail.com>
2014-07-04 21:22:04 -0400

    Add expressions for all 10 components of the second order source

--- R2.h
+++ R2.h
@@ -9,3 +9,30 @@
 std::vector<std::complex<double>> R2_1(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
   const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
   int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_2(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_3(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_4(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_5(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_6(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_7(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_8(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_9(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
+std::vector<std::complex<double>> R2_10(const std::vector<double> &r, const std::vector<double> &f, const std::vector<double> &fp,
+  const boost::multi_array<std::complex<double>,4> &h, const boost::multi_array<std::complex<double>,4> &dh,
+  int l3, int m3, int l1, int m1, int l2, int m2);
--- Ricci.cc
+++ Ricci.cc
@@ -45,7 +45,38 @@ int main(int argc, char* argv[])
         for(int m1=-l1; m1<=l1; ++m1) {
           if(abs(m3 - m1) > l2)
             continue;
-          tmp = R2_1(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+          switch(i3) {
+            case 1:
+              tmp = R2_1(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 2:
+              tmp = R2_2(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 3:
+              tmp = R2_3(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 4:
+              tmp = R2_4(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 5:
+              tmp = R2_5(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 6:
+              tmp = R2_6(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 7:
+              tmp = R2_7(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 8:
+              tmp = R2_8(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 9:
+              tmp = R2_9(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+            case 10:
+              tmp = R2_10(r, f, fp, h, dh, l3, m3, l1, m1, l2, m3-m1);
+              break;
+          }
           for(int j=0; j< tmp.size(); ++j)
             src[i3][l3][m3][j] += tmp[j];
         }
